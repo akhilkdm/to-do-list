827d611e0f561a64aa349a1221e486b36fb89728
Akhilkdm <akhilkodavalam@gmail.com>
2022-06-21 15:09:26 +0530

    updated null input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
-import './App.css';
-import { useState } from 'react'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [toDos, setToDos] = useState([])
-  const [toDo, setToDo] = useState('')
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+  const handNull = () => {
+    setToDo("");
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -15,40 +18,64 @@ function App() {
         <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
       </div>
       <div className="input">
-        <input value={toDo} onChange={(e) => setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setToDos([...toDos, {id:Date.now() ,text:toDo,status:false}])} className="fas fa-plus"></i>
+        <input
+          value={toDo} 
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() =>{if(toDo !== ''){
+            setToDos(
+              [...toDos, { id: Date.now(), text: toDo, status: false }],
+              handNull()
+            )
+          }}}
+          className="fas fa-plus"
+        ></i>
       </div>
       <div className="todos">
         {toDos.map((obj) => {
-
-
           return (
-
             <div className="todo">
               <div className="left">
-                <input onChange={(e)=>{
-                 
-                  setToDos(toDos.filter(obj2=>{
-                    if(obj2.id===obj.id){
-                      obj2.status=e.target.checked
-                    }
-                    return obj2
-                  }))
-                }} value={obj.status} type="checkbox" name="" id="" />
+                <input
+                  onChange={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
                 <p>{obj.text}</p>
               </div>
               <div className="right">
-                <i onClick={(e)=>{
-                  setToDos(toDos.filter(obj2=>{
-                    let temp;
-                    if(obj2.id!==obj.id){
-                      temp=obj2
-                    }
-                    return temp;
-                  }))
-                }}  className="fas fa-times"></i>
+                <i
+                  onClick={(e) => {
+                    setToDos(
+                      toDos.filter((obj2) => {
+                        let temp;
+                        if (obj2.id !== obj.id) {
+                          temp = obj2;
+                        }
+
+                        return temp;
+                      })
+                    );
+                  }}
+                  className="fas fa-times"
+                ></i>
               </div>
-            </div>)
+            </div>
+          );
         })}
       </div>
     </div>
